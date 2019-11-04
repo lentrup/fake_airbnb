03712f56c2684aedb6b933100bfa3f28d2d70e59
Leon Entrup <luenneberger@gmail.com>
2019-11-04 22:37:24 +0100

    added all

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'flats', to: 'flats#index', as: :index
+  get 'flats/:id', to: 'flats#show', as: :show
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
